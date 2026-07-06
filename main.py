1e155cccb5e375fc8bc50d2c0193ce4f66be9d15
rajiya82 <rajiya82@gmail.com>
2026-07-06 21:48:58 +0800

    Add functions

--- main.py
+++ main.py
@@ -1,79 +1,91 @@
 from typing import List
 
-todos = []
-while True:
-    what_to_do = input("What do you want to do? (Please enter only add/edit/show/exit)")
-    what_to_do = what_to_do.strip()
+def get_todos(filepath = "todos.txt"):
+    with open(filepath,"r") as file:
+        local_todos = file.readlines()
+        return local_todos
 
-    if what_to_do.startswith("add"):
-        with open("todos.txt","r") as file:
-            todos = file.readlines()
+def put_todos(local_todos, filepath = "todos.txt"):
+    with open(filepath,"w") as file:
+        file.writelines(local_todos)
+        return local_todos
 
-        for item in todos:
-            if str(item).lower() == str(what_to_do[4:]).lower():
-                print("This TODO already exists.")
-            break
+def update_todos(completed_idx, local_todos, filepath = "todos.txt"):
+    with open(filepath, "w") as file:
+        local_todos.pop(completed_idx)
+        file.writelines(local_todos)
+        return True
 
-        todos.append(f"{what_to_do[4:]}\n")
+def main():
+    while True:
+        what_to_do = input("What do you want to do? (Please enter only add/edit/show/exit)")
+        what_to_do = what_to_do.strip()
 
-        with open("todos.txt","w") as file:
-            file.writelines(todos)
+        if what_to_do.startswith("add"):
+            todos = get_todos()
 
-    elif what_to_do.startswith("show"):
-        with open("todos.txt","r") as file:
-            todos = file.readlines()
-        if len(todos) == 0:
-            print("No TODOs found.")
-        else:
-            for index, item in enumerate(todos, start=1):
-                print(f"{index} - {item.strip("\n")}")
-
-    elif what_to_do.startswith("edit"):
-        with open("todos.txt","r") as file:
-            todos = file.readlines()
-
-        if len(todos) == 0:
-            print("No TODOs found.")
-        else:
-            index = 1
-            print("Here are the existing TODOs")
             for item in todos:
-                print(str(index) + " - " + item.capitalize().strip())
-                index += 1
+                if str(item).lower() == str(what_to_do[4:]).lower():
+                    print("This TODO already exists.")
+                break
 
-            index_to_edit = int(input("Enter the index of the item that you want to edit: "))
-            index_to_edit = index_to_edit - 1
-            new_todo = input("Enter the new TODO: ")
-            todos[index_to_edit] = f"{new_todo} \n"
+            todos.append(f"{what_to_do[4:]}\n")
+            put_todos(todos)
 
-        with open("todos.txt","w") as file:
-            file.writelines(todos)
-    elif what_to_do.startswith("complete"):
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        elif what_to_do.startswith("show"):
+            todos = get_todos()
 
-        if len(todos) > 0:
-            print("Here are the available TODOs:")
-            for index, item in enumerate(todos, start=1):
-                print(f"{index} - {item.capitalize().strip()}")
+            if len(todos) == 0:
+                print("No TODOs found.")
+            else:
+                for index, item in enumerate(todos, start=1):
+                    print(f"{index} - {item.strip("\n")}")
 
-            completed_index = 0
+        elif what_to_do.startswith("edit"):
+            todos = get_todos()
 
-            try:
-                completed_index = int(input("Which one has completed? (please enter the index)"))
-                if 0 < completed_index & completed_index <= len(todos):
-                    completed_index = completed_index - 1
-                    removed_value = todos[completed_index]
+            if len(todos) == 0:
+                print("No TODOs found.")
+            else:
+                index = 1
+                print("Here are the existing TODOs")
+                for item in todos:
+                    print(str(index) + " - " + item.capitalize().strip())
+                    index += 1
 
-                    with open("todos.txt","w") as file:
-                        todos.pop(completed_index)
-                        file.writelines(todos)
-                        print(f"TODO {removed_value.strip()} removed from the list.")
-                else:
+                index_to_edit = int(input("Enter the index of the item that you want to edit: "))
+                index_to_edit = index_to_edit - 1
+                new_todo = input("Enter the new TODO: ")
+                todos[index_to_edit] = f"{new_todo} \n"
+
+            put_todos(todos)
+
+        elif what_to_do.startswith("complete"):
+            todos = get_todos()
+
+            if len(todos) > 0:
+                print("Here are the available TODOs:")
+                for index, item in enumerate(todos, start=1):
+                    print(f"{index} - {item.capitalize().strip()}")
+
+                completed_index = 0
+
+                try:
+                    completed_index = int(input("Which one has completed? (please enter the index)"))
+                    if 0 < completed_index & completed_index <= len(todos):
+                        completed_index = completed_index - 1
+                        removed_value = todos[completed_index]
+                        updated = update_todos(completed_index, todos)
+                        if updated:
+                            print(f"TODO {removed_value.strip()} removed from the list.")
+                    else:
+                        print("Please enter a valid number")
+                except Exception as e:
                     print("Please enter a valid number")
-            except Exception as e:
-                print("Please enter a valid number")
+            else:
+                print("No TODOs found.")
         else:
-            print("No TODOs found.")
-    else:
-        print("Invalid input.")
+            print("Invalid input.")
+
+if __name__ == "__main__":
+    main()
