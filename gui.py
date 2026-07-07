8bc62acc700929b1e92821512819ec30e6588fde
rajiya82 <rajiya82@gmail.com>
2026-07-08 00:10:22 +0800

    Add and function for the complete button

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import operations
 import FreeSimpleGUI as gui
 
+gui.theme('DarkBlue')
+
 label = gui.Text("Type in a TODO: ")
 input_box = gui.InputText(tooltip = "enter todo", key="todo")
 add_button = gui.Button("Add")
@@ -9,12 +11,12 @@ list_box = gui.Listbox(values=operations.get_todos(), key="todo_items", enable_e
 complete_button = gui.Button("Complete", key="complete")
 
 window = gui.Window("My to-do App",
-                    layout=[[label, input_box,add_button],[list_box, edit_button],[complete_button]],
+                    layout=[[label, input_box,add_button],
+                            [list_box, edit_button],
+                            [complete_button]],
                     font=('Helvetica', 16))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
     match str(event).lower():
         case "add":
             todos = operations.get_todos()
@@ -27,32 +29,37 @@ while True:
         case "todo_items":
             window['todo'].update(value=values["todo_items"][0])
         case "edit":
-            existing_todos = operations.get_todos()
-            new_todo = values["todo"] + "\n"
-            old_todo_to_edit = values["todo_items"][0]
+            try:
+                existing_todos = operations.get_todos()
+                new_todo = values["todo"] + "\n"
+                old_todo_to_edit = values["todo_items"][0]
 
-            if old_todo_to_edit in existing_todos:
-                old_index = existing_todos.index(old_todo_to_edit)
-                existing_todos[old_index] = new_todo
+                if old_todo_to_edit in existing_todos:
+                    old_index = existing_todos.index(old_todo_to_edit)
+                    existing_todos[old_index] = new_todo
 
-            operations.put_todos(existing_todos)
-            window['todo'].update('')
+                operations.put_todos(existing_todos)
+                window['todo'].update('')
 
-            new_existing_todos = operations.get_todos()
-            window['todo_items'].update(values=new_existing_todos)
+                new_existing_todos = operations.get_todos()
+                window['todo_items'].update(values=new_existing_todos)
+            except IndexError:
+                gui.popup_error("Please select an item first")
 
         case "complete":
-            existing_todos = operations.get_todos()
-            completed_todo = values["todo"]
-            if completed_todo in existing_todos:
-                existing_todos.remove(completed_todo)
+            try:
+                existing_todos = operations.get_todos()
+                completed_todo = values["todo"]
+                if completed_todo in existing_todos:
+                    existing_todos.remove(completed_todo)
 
-            operations.put_todos(existing_todos)
-            window['todo'].update('')
+                operations.put_todos(existing_todos)
+                window['todo'].update('')
 
-            new_existing_todos = operations.get_todos()
-            window['todo_items'].update(values=new_existing_todos)
-
+                new_existing_todos = operations.get_todos()
+                window['todo_items'].update(values=new_existing_todos)
+            except IndexError:
+                gui.popup("Please select an item first")
 
         case WINDOW_CLOSED:
             break
